d88b958fa21bf39c6657e0cb37c3d04f7643260c
BajcoXD <viktor.gaming2018@gmail.com>
2025-03-03 01:07:34 +0100

    Task1

---
+++ HomeWork/Homework01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿#region Task 1
+
+double result = 0;
+
+Console.WriteLine("Enter the first number:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What operation would you like to do?");
+char operation = Convert.ToChar(Console.ReadLine());
+
+if (operation == '+')
+{
+    result = num1 + num2;
+    Console.WriteLine("Result:" + result);
+}
+if (operation == '-')
+{
+    result = num1 - num2;
+    Console.WriteLine("Result:" + result);
+}
+if (operation == '*')
+{
+    result = num1 * num2;
+    Console.WriteLine("Result:" + result);
+}
+if (operation == '/')
+{
+    result = num1 / num2;
+    Console.WriteLine("Result:" + result);
+}
+else
+{
+    Console.WriteLine("Wrong input");
+}
+#endregion
