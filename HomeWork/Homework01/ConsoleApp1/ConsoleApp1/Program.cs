16769134919a5cae08ba8468c962b9a2d6777da6
BajcoXD <viktor.gaming2018@gmail.com>
2025-03-11 16:13:50 +0100

    Homework01

--- HomeWork/Homework01/ConsoleApp1/ConsoleApp1/Program.cs
+++ HomeWork/Homework01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -39,19 +39,40 @@
 
 #region Task 2
 
-Console.WriteLine("Please enter four numbers");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-int number3 = Convert.ToInt32(Console.ReadLine());
-int number4 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Please enter four numbers");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//int number4 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Caculating the avarage number...");
+//Console.WriteLine("Caculating the avarage number...");
 
-int avarageNum = (number1 + number2 + number3 + number4) / 4;
+//int avarageNum = (number1 + number2 + number3 + number4) / 4;
 
-Console.ReadLine();
-Console.WriteLine("Calculating...");
-Console.ReadLine();
-Console.WriteLine($"Your avarage number is {avarageNum}");
+//Console.ReadLine();
+//Console.WriteLine("Calculating...");
+//Console.ReadLine();
+//Console.WriteLine($"Your avarage number is {avarageNum}");
+
+#endregion
+
+#region Task 3
+
+// Prompt user for input
+Console.Write("Input the First Number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the Second Number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Swap the numbers
+int temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+// Display output after swapping
+Console.WriteLine("\nAfter Swapping:");
+Console.WriteLine("First Number: " + firstNumber);
+Console.WriteLine("Second Number: " + secondNumber);
 
 #endregion
